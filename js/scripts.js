7b4227aed7223f285ccdc8e7807d9c4a634cff9b
yl2480 <36003439+yl2480@users.noreply.github.com>
2018-02-21 17:00:01 -0500

    class_resubmit

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var London = L.marker([51.510025, -0.12428]).bindPopup ('Center of London'). addTo(map);
 var Paris = L.marker([48.856358, 2.354851]).bindPopup ('Center of Paris').addTo(map);
 var Bern = L.marker([46.944637, 7.473450]).bindPopup ('Center of Bern'). addTo(map);
-var cities = L.layerGroup([London, Paris, Bern]);
+
 
 var circleLondon = L.circle([51.510025, -0.12428], {
             color: 'red',
@@ -32,29 +32,3 @@ var circleBern = L.circle([46.944637, 7.473450], {
                   fillOpacity: 0,
                   radius: 5000
               }).addTo(map);
-
-//
-// var grayscale = L.tileLayer(mapboxUrl, {id: 'map', attribution: mapboxAttribution}),
-//     streets   = L.tileLayer(mapboxUrl, {id: 'map', attribution: mapboxAttribution});
-//
-// var map = L.map('map', {
-//     center: [39.73, -104.99],
-//     zoom: 10,
-//     layers: [grayscale, cities]
-// });
-//
-// var baseMaps = {
-//     "Grayscale": grayscale,
-//     "Streets": streets
-// };
-//
-// var overlayMaps = {
-//     "Cities": cities
-// };
-//
-// L.control.layers(baseMaps, overlayMaps).addTo(map);
-//
-// var baseMaps = {
-//     "<span style='color: gray'>Grayscale</span>": grayscale,
-//     "Streets": streets
-// };
